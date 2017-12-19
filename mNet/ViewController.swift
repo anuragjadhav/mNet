8138ef71b6729b0ee33713b9429664bad57d9b03
anuragjadhav <anuragj@timesaverz.co.in>
2017-12-19 21:52:07 +0530

    Initial commit

---
+++ mNet/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mNet
+//
+//  Created by Anurag Jadhav on 12/19/17.
+//  Copyright © 2017 mNet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
