469dad39db957477fdbecad4c8d9e10e682f80af
Nachiket Vaidya <nachiket.vaidya@neebal.com>
2018-01-11 22:44:18 +0530

    Login buttons shadow Filter screen UI start

--- mNet/Constants.swift
+++ mNet/Constants.swift
@@ -12,6 +12,8 @@ struct ColorConstants {
     
     static let kBrownColor: UIColor = UIColor(red: 97.0/255.0, green: 72.0/255.0, blue: 57.0/255.0, alpha: 1.0)
     static let kBlueColor: UIColor = UIColor(red: 74.0/255.0, green: 144.0/255.0, blue: 226.0/255.0, alpha: 1.0)
+    static let kRedColor: UIColor = UIColor(red: 221.0/255.0, green: 75.0/255.0, blue: 57.0/255.0, alpha: 1.0)
+    
 }
 
 
--- mNet/ViewBorder.swift
+++ mNet/ViewBorder.swift
@@ -49,4 +49,38 @@ extension UIView {
             layer.borderColor = newValue?.cgColor
         }
     }
+    
+//    @IBInspectable var shadowColor: UIColor? {
+//        get {
+//            return UIColor(cgColor: layer.shadowColor!)
+//        }
+//        set {
+//            layer.shadowColor = newValue?.cgColor
+//        }
+//    }
+//    @IBInspectable var shadowOffset: CGSize? {
+//        get {
+//            return layer.shadowOffset
+//        }
+//        set {
+//            layer.shadowOffset = newValue!
+//        }
+//    }
+//    @IBInspectable var shadowOpacity: Float? {
+//        get {
+//            return layer.shadowOpacity
+//        }
+//        set {
+//            layer.shadowOpacity = newValue!
+//        }
+//    }
+//    
+//    @IBInspectable var shadowRadius: CGFloat? {
+//        get {
+//            return layer.shadowRadius
+//        }
+//        set {
+//            layer.shadowRadius = newValue!
+//        }
+//    }
 }
