ed05caec13db11c4f0a16391f51557154a302806
Nachiket Vaidya <nachiket.vaidya@neebal.com>
2018-01-14 03:16:14 +0530

    Collection view in pending approvals table view cell Search bar above table view in pending approvals Filter icon Plus circle icon fix in new conversation

---
+++ mNet/PendingApprovalsCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  PendingApprovalsCollectionViewCell.swift
+//  mNet
+//
+//  Created by Nachiket Vaidya on 14/01/18.
+//  Copyright © 2018 mNet. All rights reserved.
+//
+
+import UIKit
+
+class PendingApprovalsCollectionViewCell: UICollectionViewCell {
+    
+}
