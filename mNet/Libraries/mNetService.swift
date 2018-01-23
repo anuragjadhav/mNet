e4b9afbfa06aaba40095bf6ca9953c1b0e5ab627
Nachiket Vaidya <nachiket.vaidya@neebal.com>
2018-01-24 00:12:06 +0530

    Updated code to Swift 4
    Added following Libraries:
    - Alamofire
    - Object Mapper
    - Alamofire + Object.
    
    New class mNetService to make all server calls.

---
+++ mNet/Libraries/mNetService.swift
@@ -0,0 +1,13 @@
+//
+//  mNetService.swift
+//  mNet
+//
+//  Created by Nachiket Vaidya on 24/01/18.
+//  Copyright © 2018 mNet. All rights reserved.
+//
+
+import Foundation
+
+class mNetService: NSObject {
+
+}
