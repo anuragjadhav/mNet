8fae38c3da309413bc4fb96441107f5d1904c777
Nac <nachiket@sportzinteractive.net>
2018-02-09 14:55:07 +0530

    Common Response Object Guard statements and common response in settings wrapper (only first function) Created models for dashboard stats and user applications - Both data required on Dashboard screen.

--- mNet/Constants.swift
+++ mNet/Constants.swift
@@ -28,6 +28,8 @@ struct URLS {
     static let registerDevice:URL = URL(string: "registerToken", relativeTo: liveBaseURL)!
     static let deRegisterDevice:URL = URL(string: "deregisterToken", relativeTo: liveBaseURL)!
     
+    static let getDashboardStatistics = URL(string: "GetDashBoardStatistics", relativeTo: baseURL)!
+    static let getUserAppsList = URL(string: "getUserAppList", relativeTo: baseURL)!
 }
 
 
---
+++ mNet/UserApp.swift
@@ -0,0 +1,62 @@
+//
+//  UserApp.swift
+//  mNet
+//
+//  Created by Nachiket Vaidya on 09/02/18.
+//  Copyright © 2018 mNet. All rights reserved.
+//
+
+import UIKit
+
+class UserApp: NSObject, Mappable {
+    
+    var appId:String = ""
+    var appAdminId:String = ""
+    var appURL:URL?
+    var appUserId:String = ""
+    var appType:String = ""
+    var createdOn:String = ""
+    var fabStatus:String = ""
+    var appName:String = ""
+    var appDescription:String = ""
+    var appLogoLink:String = ""
+    var appImageLink:String = ""
+    var appWidth:String = ""
+    var appHeight:String = ""
+    var allowFullScreen:String = ""
+    var applicationOpenIn:String = ""
+    var appSequence:String = ""
+    var branchName:String = ""
+    var companyName:String = ""
+    var organisationName:String = ""
+    var recieverName:String = ""
+    
+    required init?(map: Map) {
+        
+    }
+    
+    func mapping(map: Map) {
+        
+        appId <- map["app_id"]
+        appAdminId <- map["app_admin_id"]
+        appURL <- map["app_url"]
+        appUserId <- map["app_userid"]
+        appType <- map["app_type"]
+        createdOn <- map["created_on"]
+        fabStatus <- map["fab_status"]
+        appName <- map["app_name"]
+        appDescription <- map["app_description"]
+        appLogoLink <- map["app_logo_link"]
+        appImageLink <- map["app_img_link"]
+        appWidth <- map["app_width"]
+        appHeight <- map["app_height"]
+        allowFullScreen <- map["allow_fullscreen"]
+        applicationOpenIn <- map["application_open_in"]
+        appSequence <- map["app_sequence"]
+        branchName <- map["branch_name"]
+        companyName <- map["comp_name"]
+        organisationName <- map["org_name"]
+        recieverName <- map["recieverName"]
+    }
+    
+}
