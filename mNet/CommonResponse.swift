baed8153ba1d314fa0276c98fb25008c2d8edf17
Nac <nachiket@sportzinteractive.net>
2018-02-09 15:00:37 +0530

    Command line commit

---
+++ mNet/CommonResponse.swift
@@ -0,0 +1,13 @@
+//
+//  CommonResponse.swift
+//  mNet
+//
+//  Created by Nachiket Vaidya on 09/02/18.
+//  Copyright © 2018 mNet. All rights reserved.
+//
+
+import UIKit
+
+class CommonResponse: NSObject {
+
+}
