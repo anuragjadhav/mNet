4a5cda82f7cfab35811e5a341e134af20528b335
Nachiket Vaidya <nachiketvaidya@nachikets-macbook-pro.local>
2018-05-23 15:09:35 +0530

    Handled cancelled and rejected status based on object data and not on section (list)

--- mNet/ApprovalsDataController.swift
+++ mNet/ApprovalsDataController.swift
@@ -164,8 +164,8 @@ class ApprovalsDataController: NSObject {
         WrapperManager.shared.approvalWrapper.getApproval(postParams: postData, onSuccess: { (deeplinkApprovalData) in
             let section:ApprovalSection = ApprovalSection()
             section.approvalStatus = deeplinkApprovalData.approvalStatus
-            section.isRejected = self.selectedSection?.isRejected ?? false
-            section.isCancelled = self.selectedSection?.isCancelled ?? false
+            section.isRejected = deeplinkApprovalData.isRejected
+            section.isCancelled = deeplinkApprovalData.isCancelled
             self.deeplinkingApproval = deeplinkApprovalData
             self.deeplinkingApprovalSection = section
             self.isFromDeepLinking = true
